03c4addfc41b3d04bd6a9e757b7829206ccde04a
YvanThrimawithana <132426595+YvanThrimawithana@users.noreply.github.com>
2023-07-30 22:52:02 +0530

    Add files via upload

---
+++ Practical 5/employee details/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employee_details
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            employee employee = new employee();
+
+            // Take user inputs
+            Console.Write("Enter employee ID: ");
+            int empID = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter employee name: ");
+            string empName = Console.ReadLine();
+
+            Console.Write("Enter employee designation: ");
+            string empDesign = Console.ReadLine();
+
+            // Set values using setter methods
+            employee.EmpID = empID;
+            employee.EmpName = empName;
+            employee.EmpDesign = empDesign;
+
+            // Get values using getter methods
+            int retrievedEmpID = employee.EmpID;
+            string retrievedEmpName = employee.EmpName;
+            string retrievedEmpDesign = employee.EmpDesign;
+
+            // Print the message
+            Console.WriteLine("Hello, " + retrievedEmpName + "! Your employee ID is " + retrievedEmpID + " and you are a " + retrievedEmpDesign + ".");
+
+
+
+        }
+    }
+}
---
+++ Practical 5/employee details/employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employee_details
+{
+    internal class employee
+    {
+        private string id;
+        private string EmpName;
+        private string EmpDesig
+    }
+
+    public void setEmpName(string input)
+    {
+        Emp_Name = input;
+    }
+    public void setEmpDesig (string input)
+    {
+        emp_desig = input; 
+    }
+
+    public void setid(string input)
+    {
+        emp_id = input;
+    }
+}
